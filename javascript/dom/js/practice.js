1539c528c3a886b09d5b62d2d571cf73d0633de6
HaRi-Haran-D <hvj58128@gmail.com>
2025-11-01 22:54:37 +0530

    first commit

---
+++ javascript/dom/js/practice.js
@@ -0,0 +1,7 @@
+const mainTitle = document.getElementById('main-title');
+const highlightedItems=document.getElementsByClassName('highlight');
+const paragraphs = document.getElementsByTagName('p');
+
+console.log(mainTitle);
+console.log(highlightedItems);
+console.log(paragraphs);
---
+++ javascript/dom/js/query.js
@@ -0,0 +1,6 @@
+const firstHighlight=document.querySelector('.highlight');
+const allParagraphs=document.querySelectorAll('p');
+
+console.log(firstHighlight)
+
+console.log(allParagraphs)

521251b72658d424eaf0e41c371314bdcd2403ad
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-14 22:00:38 +0530

    Practice

---
+++ React/vite-project/src/Navabar.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+function Navbar(){
+
+    const [count,setCount] = useState(0)
+
+    return(
+        <div>
+            <h1>Hello World !!!</h1>
+            <button onClick={() => setCount((count) => count + 1)}>+</button>
+            <p>{count}</p>
+            <button onClick={() => setCount((count) => count - 1)}>-</button>
+        </div>
+    )
+}
+
+export default Navbar

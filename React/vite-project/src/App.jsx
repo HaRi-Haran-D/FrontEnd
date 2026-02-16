f1989c8ec5fe2aa950fdbfad627505dae90c7e28
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-16 22:02:45 +0530

    Practice

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 import Navbar from './Navabar'
 import Course from './Course'
 import Footer from './Footer'
--- React/vite-project/src/Course.jsx
+++ React/vite-project/src/Course.jsx
@@ -1,8 +1,12 @@
 const course1 = 'HTML'
 
 function Course(){
+    const style = {
+        backgroundColor:'green',
+        color : 'white'
+    }
     return(
-        <div className="card">
+        <div className="card" style={style}>
             <img src="" alt="" />
             <h3>{course1}</h3>
             <p>This is HTML</p>

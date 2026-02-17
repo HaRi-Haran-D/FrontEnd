022697f5006cc2ba186ba38f8d4c177ade889c89
HaRi-Haran-D <hvj58128@gmail.com>
2026-02-17 18:21:49 +0530

    Practice

--- React/vite-project/src/Course.jsx
+++ React/vite-project/src/Course.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react"
+import JPG from './assets/react.svg'
 const course1 = 'HTML'
 
 function Course(){
+    const [count,setCount] = useState(0)
     const style = {
         backgroundColor:'green',
         color : 'white'
     }
     return(
-        <div className="card" style={style}>
-            <img src="" alt="" />
-            <h3>{course1}</h3>
-            <p>This is HTML</p>
-        </div>
+        <section>
+            <div className="card">
+                <img src={JPG} alt="" />
+                <h3>{course1}</h3>
+                <p>This is HTML</p> 
+            </div>
+            <div>
+                <button className="btn" onClick={() => setCount((count) => count + 1)}>+</button>
+                <p>{count}</p>
+                <button className="btn" onClick={() => setCount((count) => count - 1)}>-</button>
+            </div>
+        </section>
     )
 }
 
--- React/vite-project/src/Navabar.jsx
+++ React/vite-project/src/Navabar.jsx
@@ -1,15 +1,8 @@
-import { useState } from "react"
-
 function Navbar(){
-
-    const [count,setCount] = useState(0)
 
     return(
         <div>
             <h1>Hello World !!!</h1>
-            {/* <button onClick={() => setCount((count) => count + 1)}>+</button>
-            <p>{count}</p>
-            <button onClick={() => setCount((count) => count - 1)}>-</button> */}
         </div>
     )
 }
